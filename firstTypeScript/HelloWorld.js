7a0d3e07cb9c6c56304ef21d7a2c8a4e4d28c33f
Kumar Vaduganathan <kumarvaduganathan@MacBookPro.lan>
2025-01-18 11:27:10 -0600

    All

---
+++ firstTypeScript/HelloWorld.js
@@ -0,0 +1,12 @@
+var message = "Hello Kumar";
+console.log(message);
+var grade = 85;
+if (grade >= 95) {
+    console.log("great!!!");
+}
+else if (grade >= 80) {
+    console.log("good!!");
+}
+else {
+    console.log("keep working");
+}
---
+++ firstTypeScript/HelloWorld.ts
@@ -0,0 +1,12 @@
+let message ="Hello Kumar";
+console.log(message);
+
+let grade = 85;
+
+if (grade >= 95){
+    console.log("great!!!");
+}else if (grade >= 80){
+    console.log("good!!")
+}else{
+    console.log("keep working")
+}
